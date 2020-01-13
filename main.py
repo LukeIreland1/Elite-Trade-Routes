628fb5269ab025dfef5b8603e32c863856af5a40
Luke Ireland <mi17295@essex.ac.uk>
2020-01-13 19:08:27 +0000

    Improved download and read processes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import urllib.request
-import json
 import csv
+import json
+import sys
+import urllib.request
 from pathlib import Path
+from urllib.request import urlretrieve
 
 ROOT = Path(__file__).parent
 
@@ -12,9 +14,38 @@ BODIES_URL = "https://eddb.io/archive/v6/bodies.jsonl"
 UPDATED_BODIES_URL = "https://eddb.io/archive/v6/bodies_recently.jsonl"
 STATIONS_URL = "https://eddb.io/archive/v6/stations.json" 
 FACTIONS_URL = "https://eddb.io/archive/v6/factions.json"
-PRICES_URL = "https://eddb.io/archive/v6/listings.csv"
+LISTINGS_URL = "https://eddb.io/archive/v6/listings.csv"
 COMMODITIES_URL = "https://eddb.io/archive/v6/commodities.json"
 MODULES_URL = "https://eddb.io/archive/v6/modules.json"
+
+systems = dict()
+commodities = dict()
+listings = dict()
+
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize: # near the end
+            sys.stderr.write("\n")
+    else: # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+def download(url, path):
+    if not path.exists():
+        print("Getting {} data".format(path.name.split()[0]))
+        urlretrieve(url, path, reporthook)
+    data = []
+    print("Reading {}".format(path.name))
+    with open(path, "r", encoding="utf8") as read_file:
+        if path.name.endswith(".csv"):
+            data = list(csv.DictReader(read_file))
+        elif path.name.endswith(".json"):
+            data = json.load(read_file)
+    return data
 
 cache = ROOT.joinpath("cache")
 if cache.exists():
@@ -24,23 +55,13 @@ if cache.exists():
 else:
     cache.mkdir()
 
-systems_path = cache.joinpath("systems.csv")
-if not systems_path.exists():
-    print("Getting systems data")
-    urllib.request.urlretrieve(SYSTEMS_URL, systems_path)
+listings_path = cache.joinpath("listings.csv")
+listings_data = download(LISTINGS_URL, listings_path)
 
-with open(systems_path, "r", encoding="utf8") as read_file:
-    systems_data = csv.DictReader(read_file)
+stations_path = cache.joinpath("stations.json")
+stations_data = download(stations_URL, stations_path)
 
+commodities_path = cache.joinpath("commodities.json")
+commodities_data = download(commodities_URL, commodities_path)
 
-
-
-commodities_path = cache.joinpath("commodities.csv")
-if not commodities_path.exists():
-    print("Getting commodities data")
-    urllib.request.urlretrieve(COMMODITIES_URL, commodities_path)
-
-with open(commodities_path, "r", encoding="utf8") as read_file:
-    commodities_data = json.load(read_file)
-
-print(systems_data, commodities_data)
+print(commodities_data)
