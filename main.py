d4f2eaf89796523b6cbaa33e27460ec96eaaad5a
Luke Ireland <mi17295@essex.ac.uk>
2020-01-14 00:31:25 +0000

    Fixed sorting systems and reimplemented pandas. Added system data for top 10 selling stations.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import urllib.request
 from pathlib import Path
 from urllib.request import urlretrieve
 from operator import itemgetter
+import pandas as pd
+import pickle
 
 ROOT = Path(__file__).parent
 
@@ -39,7 +41,7 @@ def reporthook(blocknum, blocksize, totalsize):
 
 def download(url, path):
     if not path.exists():
-        print("Getting {} data".format(path.name.split()[0]))
+        print("Downloading {} data".format(path.name.split()[0]))
         urlretrieve(url, path, reporthook)
     data = []
     print("Reading {}".format(path.name))
@@ -59,22 +61,66 @@ if cache.exists():
 else:
     cache.mkdir()
 
-listings_path = cache.joinpath("listings.csv")
-listings_data = download(LISTINGS_URL, listings_path)
+pickles = ROOT.joinpath("pickles")
+if pickles.exists():
+    if not pickles.is_dir():
+        print("Could not create pickles directory")
+        exit(1)
+else:
+    pickles.mkdir()
 
-stations_path = cache.joinpath("stations.json")
-# stations_data = download(STATIONS_URL, stations_path)
+listings_pickle = pickles.joinpath("listings")
+if listings_pickle.exists():
+    listings_data = pickle.load(open(listings_pickle, "rb"))
+else:
+    listings_path = cache.joinpath("listings.csv")
+    listings_data = download(LISTINGS_URL, listings_path)
+    pickle.dump(listings_data, open(listings_pickle, "wb"))
 
-commodities_path = cache.joinpath("commodities.json")
-commodities_data = download(COMMODITIES_URL, commodities_path)
+stations_pickle = pickles.joinpath("stations")
+if stations_pickle.exists():
+    stations_data = pickle.load(open(stations_pickle, "rb"))
+else:
+    stations_path = cache.joinpath("stations.json")
+    stations_data = download(STATIONS_URL, stations_path)
+    pickle.dump(stations_data, open(stations_pickle, "wb"))
 
-mgf_listings = []
+commodities_pickle = pickles.joinpath("commodities")
+if commodities_pickle.exists():
+    commodities_data = pickle.load(open(commodities_pickle, "rb"))
+else:
+    commodities_path = cache.joinpath("commodities.json")
+    commodities_data = download(COMMODITIES_URL, commodities_path)
+    pickle.dump(commodities_data, open(commodities_pickle, "wb"))
 
-for listing in listings_data:
-    if listing["commodity_id"] == "304":
-        listing["sell_price"] = int(listing["sell_price"])
-        mgf_listings.append(listing)
 
-mgf_listings = sorted(mgf_listings, key=itemgetter('sell_price'), reverse=True)[:10]
+populated_systems_pickle = pickles.joinpath("populated_systems")
+if populated_systems_pickle.exists():
+    populated_systems_data = pickle.load(open(populated_systems_pickle, "rb"))
+else:
+    populated_systems_path = cache.joinpath("systems_populated.json")
+    populated_systems_data = download(
+        POPULATED_SYSTEMS_URL, populated_systems_path)
+    pickle.dump(populated_systems_data, open(populated_systems_pickle, "wb"))
 
-print(mgf_listings)
+listings = pd.DataFrame(listings_data)
+stations = pd.DataFrame(stations_data)
+commodities = pd.DataFrame(commodities_data)
+populated_systems = pd.DataFrame(populated_systems_data)
+
+mgf_listings = listings[listings.commodity_id == "304"]
+
+mgf_listings.sell_price = mgf_listings.sell_price.astype(int)
+mgf_listings.buy_price = mgf_listings.buy_price.astype(int)
+
+sell_listings = mgf_listings.sort_values(
+    by="sell_price", ascending=False).head(10)
+buy_listings = mgf_listings.sort_values(by="buy_price", ascending=False)
+
+sell_stations = stations.loc[stations["id"].isin(sell_listings.station_id)]
+buy_stations = stations.loc[stations["id"].isin(buy_listings.station_id)]
+
+sell_systems = populated_systems.loc[populated_systems["id"].isin(sell_stations.system_id)]
+buy_systems = populated_systems.loc[populated_systems["id"].isin(buy_stations.system_id)]
+
+print(sell_systems)
